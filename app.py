fa87bbe6485e25a3fb2119587b32e5ee91a225ad
Kraljiccar <nemanjakraljic@gmail.com>
2020-09-07 17:24:27 +0000

    index page

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'Recipe'
+app.config["MONGO_URI"] = 'mongodb+srv://root1:r00tUser@myfirstcluster.of3gn.mongodb.net/<recipe>?retryWrites=true&w=majority'
+
+
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+#displaying main page
+@app.route('/recipes')
+def index_recipe():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
