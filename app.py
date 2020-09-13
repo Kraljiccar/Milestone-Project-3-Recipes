d9dac09dcbaba7195af0c1c36aea03243c31ac53
Kraljiccar <nemanjakraljic@gmail.com>
2020-09-13 09:45:13 +0000

    doing it on another way after talkning to my mentor

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash
-import bcrypt
+
 
 if os.path.exists("env.py"):
     import env
@@ -28,30 +27,6 @@ mongo = PyMongo(app)
 @app.route('/recipes')
 def index_recipe():
     return render_template('index.html')
-
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-   
-    if request.method == 'POST':
-        users = mongo.db.users
-        existing_user = users.find_one({'username':
-                                        request.form.get('username').lower()})
-        if existing_user is None:
-            hash_password = bcrypt.hashpw(
-                request.form['pasword'].encode('utf-8'), bcrypt.gensalt())
-            users.insert_one({'username': request.form['username'],
-                             'password': hash_password})
-            session['username'] = request.form['username']
-            return redirect(url_for('index_recipe'))
-        
-    return render_template('register.html', login_page=True)
-
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-   
-    return render_template('login.html', login_page=True)
-
 
 
 if __name__ == '__main__':
