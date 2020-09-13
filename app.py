9c1adb4aa6228b88e0a8446ac179637d605d15ad
Kraljiccar <nemanjakraljic@gmail.com>
2020-09-13 18:53:59 +0000

    got stuck

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import (
     Flask, flash, render_template,
-    redirect, request, session, url_for)
+    redirect, request, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -10,19 +10,12 @@ if os.path.exists("env.py"):
     import env
 
 
-app = Flask(__name__)
-
-
-
+app = Flask(__name__) 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo = PyMongo(app)
-
-
-
-
+mongo = PyMongo(app) 
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -31,22 +24,17 @@ def index():
 def get_all(category):
     if category == "all":
         category = "All recipes"
-        recipes=mongo.db.recipe.find() 
-    elif category == "other":
-        recipe = mongo.db.recipe.find({"category_name": "Other"})
+        recipe = mongo.db.recipe.find() 
     else:
         recipe = mongo.db.recipe.find({"$text": {"$search": category}})
     return render_template("recipes.html", recipe=recipe, page_title=category)
     
-
-@app.route('/view_recipe/<recipe_id>')
+@app.route('/recipe/<recipe_id>')
 def get_recipe(recipe_id):
     recipe = mongo.db.recipe.find_one({"_id": ObjectId(recipe_id)})
     return render_template('viewrecipe.html')
-
-
-
-
+    
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
