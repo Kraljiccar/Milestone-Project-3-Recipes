c646ee8ba5fb907da0ce4568fb3936229de857e3
Kraljiccar <nemanjakraljic@gmail.com>
2020-09-14 17:08:17 +0000

    add recipe works but still not as it should

--- app.py
+++ app.py
@@ -32,9 +32,21 @@ def get_all(category):
 @app.route('/recipe/<recipe_id>')
 def get_recipe(recipe_id):
     recipe = mongo.db.recipe.find_one({"_id": ObjectId(recipe_id)})
-    return render_template('viewrecipe.html')
-    
-    
+    categories = mongo.db.categories.find()
+    return render_template("viewrecipe.html", recipes=recipes, categories=categories)
+
+@app.route('/add_recipe')
+def add_recipe():
+    categories = mongo.db.categories.find()
+    return render_template("addrecipe.html", categories=categories, page_title="Add Recipe")
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipe = mongo.db.recipe
+    recipe.insert_one(request.form.to_dict())
+    return render_template('recipes.html')
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
